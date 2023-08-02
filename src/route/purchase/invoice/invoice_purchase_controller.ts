636312cfaa58c8203450b71f21fbc24897ec5ac1
Juan D. Ortiz <juanortizbayona@gmail.com>
2023-08-01 21:33:24 -0500

    Add getAll, getByRange, addAccountingSupport

--- src/route/purchase/invoice/invoice_purchase_controller.ts
+++ src/route/purchase/invoice/invoice_purchase_controller.ts
@@ -18,6 +18,112 @@ import {
 export const sheetName = "main";
 
 /**
+ * Parse data.
+ * @param {any} startPosition start position of sheet value.
+ * @param {any} values start position of sheet value.
+ */
+function parseData(
+  startPosition: number,
+  //endPosition: number,
+  values: [any]
+): any[] {
+  const data = values.map((item: any, index: number) => {
+    return {
+      position: (startPosition + index),
+      invoice: {
+        id: item[0],
+        date: item[1],
+        invoiceDate: item[2],
+        observations: item[3],
+        typeInvoice: item[4],
+        provider: item[5],
+        paymentType: item[6],
+        invoiceNumber: item[7],
+        activityMaterial: item[8],
+        price: item[9],
+        quantity: item[10],
+        chapter: item[11],
+        photoInvoice: {
+          fileId: item[12],
+        },
+        withholdingTax: item[13],
+        iva: item[14],
+        photoAccountingSupport: {
+          fileId: item[15],
+        }
+      }
+    };
+  });
+
+  return data;
+}
+
+/**
+ * Get all order purchase.
+ * @param {any} spreadsheetId spreadsheetId value.
+ */
+export async function getAll(
+  spreadsheetId: string,
+): Promise<any> {
+  const sheets = getSheetInstance();
+  const sheetResponse = await sheetsGet(
+    sheets,
+    googleAuth,
+    spreadsheetId,
+    sheetName + "!A3:P",
+  );
+
+  validateSheetResponse(sheetResponse);
+
+  const rangeSplited = sheetResponse.data.range.split(':');
+  const rangeStartPosition = parseInt(rangeSplited[0].split('!')[1].split(/[^\d]+/).join(""));
+  //const rangeEndPosition = rangeSplited[1].split(/[^\d]+/).join("");
+
+  const data = parseData(
+    rangeStartPosition, 
+    sheetResponse.data.values
+  );
+
+  const response = {
+    data: data,
+  };
+
+  return response;
+}
+
+/**
+ * Get by range.
+ * @param {any} spreadsheetId spreadsheetId value.
+ * @param {string} position position of sheet.
+ */
+export async function getByRange(
+  spreadsheetId: string,
+  position: string,
+): Promise<any> {
+  const sheets = getSheetInstance();
+  const range = sheetName + "!A" + position + ":P" + position;
+  const sheetResponse = await sheetsGet(
+    sheets,
+    googleAuth,
+    spreadsheetId,
+    range,
+  );
+
+  validateSheetResponse(sheetResponse);
+
+  const data = parseData(
+    parseInt(position), 
+    sheetResponse.data.values
+  );
+
+  const response = {
+    data: data[0],
+  };
+
+  return response;
+}
+
+/**
  * Update invoice purchase.
  * @param {any} spreadsheetId spreadsheetId value.
  * @param {any} payload payload value.
@@ -182,6 +288,73 @@ export async function update(
       googleAuth,
       spreadsheetId,
       sheetName + "!A" + payload.position + ":P",
+      rows,
+    )
+  );
+
+  const response = {
+    data: {
+      id: invoice.id,
+    },
+  };
+
+  return response;
+}
+
+/**
+ * Add accounting support to the invoice purchase.
+ * @param {any} spreadsheetId spreadsheetId value.
+ * @param {any} payload payload value.
+ */
+export async function addAccountingSupport(
+  spreadsheetId: string,
+  payload: any,
+): Promise<any> {
+  const invoice = payload.invoice;
+  const sheets = getSheetInstance();
+  const driveService = getDriveInstance();
+
+  const rows: any[][] = [];
+
+  if(invoice.photoAccountingSupport.fileId !== "") {
+    await deleteFile(
+      driveService,
+      invoice.photoAccountingSupport.fileId,
+    );
+  }
+
+  const folderResponse = await sheetsGet(
+    sheets,
+    googleAuth,
+    spreadsheetId,
+    sheetName + "!E1:F1", 
+  );
+
+  validateSheetResponse(folderResponse);
+
+  const folderId = folderResponse.data.values[0][1];
+
+  const fileExtension = invoice.photoAccountingSupport.mimeType.split('/')[1];
+  const filenamePhoto = invoice.id + "-factura-soporte-contable." + fileExtension;
+
+  const photoResponse = await uploadFile(
+    driveService,
+    folderId,
+    filenamePhoto,
+    invoice.photoAccountingSupport.mimeType,
+    invoice.photoAccountingSupport.rawData,
+  );
+
+  rows.push([
+    photoResponse.id,
+  ]);
+
+  validateSheetResponse(
+    await sheetUpdateRows(
+      sheets,
+      googleAuth,
+      spreadsheetId,
+      sheetName + "!P" + payload.position,
       rows,
     )
   );
--- src/route/purchase/invoice/invoice_purchase_route.ts
+++ src/route/purchase/invoice/invoice_purchase_route.ts
@@ -1,6 +1,10 @@
-import { HEADER_KEYS } from "../../../util/network_util";
+import {HEADER_KEYS} from "../../../util/network_util";
 import {
-  append, update,
+  append, 
+  update, 
+  addAccountingSupport,
+  getAll,
+  getByRange,
 } from "./invoice_purchase_controller";
 
 import express = require("express");
@@ -13,6 +17,55 @@ export const route = express.Router();
 
 route.use((_req: any, _res: any, next: () => void) => {
   next();
+});
+
+/**
+ * Get all
+ * Developed by Juan Ortiz
+ */
+route.get(
+  "/",
+  async (req: any, res: any) => {
+    try {
+      console.log("Purchase Invoice | Get all");
+      console.log(`RequestBody: ${JSON.stringify(req.body)}`);
+
+      const spreadsheetId = req.get(HEADER_KEYS.spreadsheetId);
+
+      const response = await getAll(
+        spreadsheetId
+      );
+
+      res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send();
+    }
+  }
+);
+
+/**
+ * Get by range
+ * Developed by Juan Ortiz
+ */
+route.get("/range", async (req: any, res: any) => {
+  try {
+    const spreadsheetId = req.get(HEADER_KEYS.spreadsheetId);
+    const position = req.query.position;
+
+    if (position !== undefined) {
+      const response = await getByRange(
+        spreadsheetId,
+        position
+      );
+      res.status(200).send(response);
+    } else {
+      throw Error("No position found!");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
 });
 
 /**
@@ -57,6 +110,33 @@ route.post(
       const payload = req.body.data;
 
       const response = await update(
+        spreadsheetId,
+        payload
+      );
+
+      res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send();
+    }
+  }
+);
+
+/**
+ * Add accounting support
+ * Developed by Juan Ortiz
+ */
+route.put(
+  "/accountingsupport",
+  async (req: any, res: any) => {
+    try {
+      console.log("Purchase Invoice | Append row");
+      console.log(`RequestBody: ${JSON.stringify(req.body)}`);
+
+      const spreadsheetId = req.get(HEADER_KEYS.spreadsheetId);
+      const payload = req.body.data;
+
+      const response = await addAccountingSupport(
         spreadsheetId,
         payload
       );
