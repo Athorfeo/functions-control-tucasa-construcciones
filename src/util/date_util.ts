1c711f4005f0249719ae352220929b15dd722b83
Juan D. Ortiz <juanortizbayona@gmail.com>
2023-10-31 23:54:55 -0500

    Update date_util.ts

--- src/util/date_util.ts
+++ src/util/date_util.ts
@@ -1,15 +1,22 @@
 /**
  * Get current date.
- * @return {string} current date formar dd/mm/yyyy.
+ * @return {string} current date formar yyyy-mm-dd.
  */
 export function getDateFormatted(): string {
-  const rawDate = new Date();
+  const rawDate = new Date(
+    new Date().toLocaleString(
+      "en-US",
+      {timeZone: "America/Bogota"},
+    )
+  );
   const year = `${rawDate.getFullYear()}`;
   let month = "";
   let day = "";
 
   if ( (rawDate.getMonth() + 1) < 10 ) {
     month = "0" + (rawDate.getMonth() + 1);
+  } else {
+    month = `${(rawDate.getMonth() + 1)}`;
   }
 
   if ( rawDate.getDate() < 10 ) {
